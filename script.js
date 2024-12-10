72ecc1ecf0d21676e9ab859fe0abd154d7e9139e
zeynab jalalian <zjalalianweb@gmail.com>
2024-12-10 13:10:58 +0330

    run

---
+++ script.js
@@ -0,0 +1,24 @@
+alert('این یک شبیه ساز ماشین حساب با چهار عملیات اصلی ریاضی است')
+alert('این مینی پروژه بسیار مبتدی است!بزن بریم')
+let n1=prompt('عدد اول را وارد نمایید.')
+let n2=prompt('عدد دوم را وارد نمایید.')
+let k=prompt('یکی از عملیات های ریاضی مقابل را وارد کنید:(+ - ÷ ×)')
+n1=Number(n1)
+n2=Number(n2)
+switch (k){
+    case "+":
+    alert(n1+n2);
+    break;
+
+    case "-":
+    alert(n1-n2)
+    break;
+
+    case "×":
+    alert(n1*n2)
+    break;
+
+    case "÷":
+   alert(n1/n2)
+    break;
+}
